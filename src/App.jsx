cbea974f9aefb754c127a10f54f666f0877f29b6
shadoeee <balaganesh541@gmail.com>
2024-04-30 01:20:48 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  // const url = 'http://localhost:4000';
+  const url = 'https://food-delivery-backend-53dlle6gu-shadoeees-projects.vercel.app/';
 
   return (
     <div>
